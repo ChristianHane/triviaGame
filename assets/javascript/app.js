451fc636592e40175e9bca1a3864c4cd9f745f8e
Christian <christian_hane@aol.com>
2017-12-25 14:22:22 -0800

    Finished most of the functionality fot the game
    Couldnt finish the timer functionality to finish the game at 0:00

---
+++ assets/javascript/app.js
@@ -0,0 +1,90 @@
+$("#start").on("click", function(){
+    $(".hidden").css("display", "block");
+    $("#start").css("display", "none");
+    timer();
+    return;
+});
+
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+
+$("#done").on("click", function(){
+    gameOver();
+});
+
+function gameOver(){    
+    if(document.getElementById("correctQ1").checked){
+        correctAnswers++;
+    }else{
+        incorrectAnswers++;
+    };
+    if(document.getElementById("correctQ2").checked){
+        correctAnswers++;
+    }else{
+        incorrectAnswers++;
+    };
+    if(document.getElementById("correctQ3").checked){
+        correctAnswers++;
+    }else{
+        incorrectAnswers++;
+    };
+    if(document.getElementById("correctQ4").checked){
+        correctAnswers++;
+    }else{
+        incorrectAnswers++;
+    };
+    if(document.getElementById("correctQ5").checked){
+        correctAnswers++;
+    }else{
+        incorrectAnswers++;
+    };
+    $("#incorrect").append(incorrectAnswers);
+    $("#correct").append(correctAnswers);               
+    $(".done").css("display", "block");
+    $(".hidden").css("display", "none");
+};
+
+$("#restart").on("click", function(){
+    location.reload();
+});
+
+function timer(){
+    document.getElementById("timer").innerHTML = 0 + ":" + 02;
+    startTimer();
+};
+
+function startTimer() {
+    var presentTime = document.getElementById("timer").innerHTML;
+    var timeArray = presentTime.split(/[:]+/);
+    var minutes = timeArray[0];
+    var seconds = checkSecond((timeArray[1] - 1));
+    if(seconds==59){
+        minutes=minutes-1
+    }
+
+    document.getElementById("timer").innerHTML =
+    minutes + ":" + seconds;
+    var timeOut = setTimeout(startTimer, 1000);
+
+    for(i = 0; i < ; i++){
+        if($("#timer") === "0:00"){
+            console.log("done")
+            gameOver();
+        };
+    }
+};
+
+function checkSecond(sec) {
+    if (sec < 10 && sec >= 0){
+        sec = "0" + sec
+    };
+    if (sec < 0){
+        sec = "59"
+    };
+    return sec;
+};
+
+
+
+
+
